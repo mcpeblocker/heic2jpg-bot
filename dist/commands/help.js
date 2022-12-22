2e43f830b90a3842ec4cad1eedb49639e6627087
Alisher Ortiqov <mcpeblocker@gmail.com>
2022-12-22 21:36:17 +0500

    rm dist build folder

--- dist/commands/help.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const bot_1 = __importDefault(require("../core/bot"));
-bot_1.default.command("help", (ctx) => {
-    ctx.reply("Xullas shunaqa shunaqa. Fayllarni yuborasiz ketma-ket, keyin /done buyruqini yuborsayiz barcha yuborgan fayllarizni convert qiberaman.");
-});
